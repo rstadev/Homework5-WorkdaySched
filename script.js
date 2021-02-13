2c20ab401564bab1b54f04762d175473c792211b
unknown <nukasine@gmail.com>
2021-02-12 19:08:17 -0500

    Did quite a bit. Added all 8 hours on the page itself, added a save button that in its current state could be considered in prototype, as they aren't finished yet but should function far enough for me to finish the program itself. Changed some css so that when past present and future classes are added to objects, that they change color and aren't all white.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-var now = moment().format("dddd, MMMM Do YYYY");
+//Using let and const because I'd like to get into this habit, and we learned this in class not too long ago.
+var now = moment().format("h a,dddd, MMMM Do YYYY");
 $("#currentDay").html(now);
-console.log(now)
+console.log(now);
 
 $(document).ready(function() {    
     function trackTime() {
@@ -8,4 +9,25 @@ $(document).ready(function() {
         console.log(currentTime)
     }
     trackTime();
-})
+});
+
+var localStorageTasks = {
+    "9": [],
+    "10": [],
+    "11": [],
+    "12": [],
+    "13": [],
+    "14": [],
+    "15": [],
+    "16": [],
+    "17": [],
+};
+
+function localStorageAdd() {
+    localStorage.setItem("tasks", JSON.stringify(localStorageTasks))
+};
+
+function localStorageGet() {
+    let getTasks = JSON.parse(localStorage.getItem("tasks"));
+    console.log(getTasks)
+};
