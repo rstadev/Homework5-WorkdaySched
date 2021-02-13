98800686ac9a7134e895e13e7798dc3f97e94d75
unknown <nukasine@gmail.com>
2021-02-12 23:34:57 -0500

    at a bit of a crossroads here. Still want to implement local Storage as an array database, but may have to opt for quicker function. Changed hour id to be on the parent div to better be targeted. Added partial save button function, needs to be expanded into local storage.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //Using let and const because I'd like to get into this habit, and we learned this in class not too long ago.
-var now = moment().format("h a,dddd, MMMM Do YYYY");
+var now = moment().format("h a , dddd , MMMM Do YYYY");
 $("#currentDay").html(now);
 console.log(now);
 
-$(document).ready(function() {    
-    function trackTime() {
-        var currentTime = moment().hour();
-        console.log(currentTime)
-    }
-    trackTime();
-});
+// $(document).ready(function() {    
+    // function trackTime() {
+let currentTime = moment().hour();
+    console.log(currentTime)
+//     }
+//     trackTime();
+// });
 
 var localStorageTasks = {
     "9": [],
@@ -26,8 +26,34 @@ var localStorageTasks = {
 function localStorageAdd() {
     localStorage.setItem("tasks", JSON.stringify(localStorageTasks))
 };
+localStorageAdd();
 
 function localStorageGet() {
     let getTasks = JSON.parse(localStorage.getItem("tasks"));
     console.log(getTasks)
 };
+localStorageGet();
+
+$(document).ready(function(){
+    $(".saveBtn").click(function(){
+        let timeVal = $(this).siblings().attr("id");
+        let textVal = $(".description").val();
+        console.log(timeVal)
+        console.log(textVal)
+    });
+});
+// function colorAssign() {
+//     switch (id = currentTime) {
+//         case ""
+
+//     }
+// }
+
+
+// function getTextArea() {
+//     $(document).ready(function() {
+//         let textVal = $.trim($("#9").val());
+//         console.log(textVal)
+//     })
+// };
+// getTextArea();
